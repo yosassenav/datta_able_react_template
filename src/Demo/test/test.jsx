7a22c848a80d71e12391f10ab0348fb8ebc1a2eb
vanessa <mromerovanessa.soy@gmail.com>
2021-08-17 10:08:55 -0500

    Adding new route + new menu item + new component

---
+++ src/Demo/test/test.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/_Aux";
+
+const test = () => {
+  return (
+    <Aux>
+      <h1>Hello World!</h1>
+      <p>dfvdfv</p>
+    </Aux>
+  );
+};
+
+export default test;
